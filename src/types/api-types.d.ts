86626b3c7e810b08fa6b064e63f1702d0f7ef9dd
sokhunter <emylivaque@gmail.com>
2022-08-05 13:13:43 -0500

    add swagger documentation

---
+++ src/types/api-types.d.ts
@@ -0,0 +1,16 @@
+export interface Planet {
+    nombre: string;
+    diametro: string;
+    periodo_rotacion: string;
+    periodo_orbita: string;
+    clima: string;
+    gravedad: string;
+    terreno: string;
+    superficie_agua: string;
+    poblacion: string;
+    url: string;
+    f_creacion: string;
+    f_edicion: string;
+}
+
+export { Planet };
